7ded0f2f803f1dca3f1ee96559b4817fb15b7fa6
sroumard <sroumard@gmail.com>
2024-12-13 07:32:49 +0100

    gerer la base de donnée de Admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Client, Itineraire, Hotel, Deplacement, ReservationHotel, Activite
+# Enregistrer les modèles dans l'administration
+admin.site.register(Client)
+admin.site.register(Itineraire)
+admin.site.register(Hotel)
+admin.site.register(Deplacement)
+admin.site.register(ReservationHotel)
+admin.site.register(Activite)
